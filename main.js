3ac28ec99692ac150c59921a45ee0347044a5b62
Tanner Eustice <teustice@msn.com>
2022-03-23 14:40:12 -0700

    fix the world

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
+const data = require('./api/spells');
 const config = require('./config');
 const discord = require('discord.js');
 const axios = require('axios')
 const { MessageEmbed } = require('discord.js');
 const { MessageActionRow, MessageButton } = require('discord.js');
 const client = new discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-
-const getSpell = () => {
-  return axios.get(`http://localhost:8088/spell`)
-}
 
 client.on("ready", () => {
   console.log("WITNESS ME")
@@ -28,51 +25,42 @@ client.on("messageCreate", msg => {
   }
 
   if (msg.content.startsWith("!dnd ")) {
-
     let foundSpell = []
-    getSpell().then(res => {
-      res.data.forEach(element => {
-        if (element.name.toLowerCase() === msg.content.split("!dnd ")[1].toLowerCase()) {
-          foundSpell = element
-        }
-      })
-
-      console.log(foundSpell);
-
-    }).then(() => {
-      let showEntries = `${foundSpell.entries.reduce((accum, curVal) => {
-        if (curVal.items){
-          return accum += `\n \n-${curVal.items.join("\n \n-")}\n`
-        } else if (curVal.entries){
-          return accum += `\n \n-${curVal.entries.join("\n \n-")}\n`
-        } else if (curVal.type === 'table'){
-          return accum += `\n \n a goddamn table you roll on? you fuckin kiddin me im not coding that shit for free \n`
-        } else {
-          return accum += `\n\n${curVal}`
-        }
-      })}`
-
-      const exampleEmbed = new MessageEmbed()
-        .setColor('#A7171A')
-        .setTitle(`${foundSpell.name}\n*${foundSpell.school} ${foundSpell.level}* ${foundSpell.meta ? "(ritual)" : ''}
-      `)
-        .setThumbnail('https://thumbs.gfycat.com/EnlightenedTalkativeCapybara-max-1mb.gif')
-        .addFields(
-          { name: 'Casting Time', value: `${foundSpell.time[0].number} ${foundSpell.time[0].unit}`, inline: true },
-          { name: 'Range', value: `${foundSpell.range.distance.amount ? foundSpell.range.distance.amount : ''} ${foundSpell.range.distance.type}`, inline: true },
-          { name: 'Duration', value: `${foundSpell.duration[0].type === "instant" ? foundSpell.duration[0].type : foundSpell.duration[0].duration.amount + " " + foundSpell.duration[0].duration.type} ${foundSpell.duration[0].concentration ? "(concentration)" : ""}`, inline: true },
-          { name: 'Components', value: `${foundSpell.components.v ? "V" : ""} ${foundSpell.components.s ? "S" : ""} ${checkForM(foundSpell)}`, inline: true },
-        )
-        .setDescription(showEntries)
-      msg.reply(({ embeds: [exampleEmbed] }))
+    data.spellData.forEach(element => {
+      if (element.name.toLowerCase() === msg.content.split("!dnd ")[1].toLowerCase()) {
+        foundSpell = element
+      }
     })
+    let showEntries = `${foundSpell.entries.reduce((accum, curVal) => {
+      if (curVal.items) {
+        return accum += `\n \n-${curVal.items.join("\n \n-")}\n`
+      } else if (curVal.entries) {
+        return accum += `\n \n-${curVal.entries.join("\n \n-")}\n`
+      } else if (curVal.type === 'table') {
+        return accum += `\n \n a goddamn table you roll on? you fuckin kiddin me im not coding that shit for free \n`
+      } else {
+        return accum += `\n\n${curVal}`
+      }
+    })}`
+
+    const exampleEmbed = new MessageEmbed()
+      .setColor('#A7171A')
+      .setTitle(`${foundSpell.name}\n*${foundSpell.school} ${foundSpell.level}* ${foundSpell.meta ? "(ritual)" : ''}
+    `)
+      .setThumbnail('https://thumbs.gfycat.com/EnlightenedTalkativeCapybara-max-1mb.gif')
+      .addFields(
+        { name: 'Casting Time', value: `${foundSpell.time[0].number} ${foundSpell.time[0].unit}`, inline: true },
+        { name: 'Range', value: `${foundSpell.range.distance.amount ? foundSpell.range.distance.amount : ''} ${foundSpell.range.distance.type}`, inline: true },
+        { name: 'Duration', value: `${foundSpell.duration[0].type === "instant" ? foundSpell.duration[0].type : foundSpell.duration[0].duration.amount + " " + foundSpell.duration[0].duration.type} ${foundSpell.duration[0].concentration ? "(concentration)" : ""}`, inline: true },
+        { name: 'Components', value: `${foundSpell.components.v ? "V" : ""} ${foundSpell.components.s ? "S" : ""} ${checkForM(foundSpell)}`, inline: true },
+      )
+      .setDescription(showEntries)
+    msg.reply(({ embeds: [exampleEmbed] }))
   }
 })
 
 client.login(config.token)
 
-
-
-// .catch(error => {
-//   msg.reply("Invalid spell name.")
-// })
+  .catch(error => {
+    msg.reply("Invalid spell name.")
+  })
