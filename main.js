91d27d8d2cc728e5a008e7e29e6a87bb4392933e
Tanner Eustice <teustice@msn.com>
2022-03-23 14:51:37 -0700

    update spells json to no longer use server

--- main.js
+++ main.js
@@ -23,37 +23,42 @@ client.on("messageCreate", msg => {
   }
 
   if (msg.content.startsWith("!dnd ")) {
-    let foundSpell = []
+    let foundSpell;
     data.spellData.forEach(element => {
       if (element.name.toLowerCase() === msg.content.split("!dnd ")[1].toLowerCase()) {
-        foundSpell = element
+        foundSpell = element;
       }
     })
-    let showEntries = `${foundSpell.entries.reduce((accum, curVal) => {
-      if (curVal.items) {
-        return accum += `\n \n-${curVal.items.join("\n \n-")}\n`
-      } else if (curVal.entries) {
-        return accum += `\n \n-${curVal.entries.join("\n \n-")}\n`
-      } else if (curVal.type === 'table') {
-        return accum += `\n \n a goddamn table you roll on? you fuckin kiddin me im not coding that shit for free \n`
-      } else {
-        return accum += `\n\n${curVal}`
-      }
-    })}`
 
-    const exampleEmbed = new MessageEmbed()
-      .setColor('#A7171A')
-      .setTitle(`${foundSpell.name}\n*${foundSpell.school} ${foundSpell.level}* ${foundSpell.meta ? "(ritual)" : ''}
-    `)
-      .setThumbnail('https://thumbs.gfycat.com/EnlightenedTalkativeCapybara-max-1mb.gif')
-      .addFields(
-        { name: 'Casting Time', value: `${foundSpell.time[0].number} ${foundSpell.time[0].unit}`, inline: true },
-        { name: 'Range', value: `${foundSpell.range.distance.amount ? foundSpell.range.distance.amount : ''} ${foundSpell.range.distance.type}`, inline: true },
-        { name: 'Duration', value: `${foundSpell.duration[0].type === "instant" ? foundSpell.duration[0].type : foundSpell.duration[0].duration.amount + " " + foundSpell.duration[0].duration.type} ${foundSpell.duration[0].concentration ? "(concentration)" : ""}`, inline: true },
-        { name: 'Components', value: `${foundSpell.components.v ? "V" : ""} ${foundSpell.components.s ? "S" : ""} ${checkForM(foundSpell)}`, inline: true },
-      )
-      .setDescription(showEntries)
-    msg.reply(({ embeds: [exampleEmbed] }))
+    if (!foundSpell) {
+      msg.reply("Invalid spell name.");
+    } else {
+      let showEntries = `${foundSpell.entries.reduce((accum, curVal) => {
+        if (curVal.items) {
+          return accum += `\n \n-${curVal.items.join("\n \n-")}\n`
+        } else if (curVal.entries) {
+          return accum += `\n \n-${curVal.entries.join("\n \n-")}\n`
+        } else if (curVal.type === 'table') {
+          return accum += `\n \n a goddamn table you roll on? you fuckin kiddin me im not coding that shit for free \n`
+        } else {
+          return accum += `\n\n${curVal}`
+        }
+      })}`
+
+      const exampleEmbed = new MessageEmbed()
+        .setColor('#A7171A')
+        .setTitle(`${foundSpell.name}\n*${foundSpell.school} ${foundSpell.level}* ${foundSpell.meta ? "(ritual)" : ''}
+      `)
+        .setThumbnail('https://thumbs.gfycat.com/EnlightenedTalkativeCapybara-max-1mb.gif')
+        .addFields(
+          { name: 'Casting Time', value: `${foundSpell.time[0].number} ${foundSpell.time[0].unit}`, inline: true },
+          { name: 'Range', value: `${foundSpell.range.distance.amount ? foundSpell.range.distance.amount : ''} ${foundSpell.range.distance.type}`, inline: true },
+          { name: 'Duration', value: `${foundSpell.duration[0].type === "instant" ? foundSpell.duration[0].type : foundSpell.duration[0].duration.amount + " " + foundSpell.duration[0].duration.type} ${foundSpell.duration[0].concentration ? "(concentration)" : ""}`, inline: true },
+          { name: 'Components', value: `${foundSpell.components.v ? "V" : ""} ${foundSpell.components.s ? "S" : ""} ${checkForM(foundSpell)}`, inline: true },
+        )
+        .setDescription(showEntries)
+      msg.reply(({ embeds: [exampleEmbed] }))
+    }
   }
 })
 
